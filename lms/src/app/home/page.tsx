a1d1893f0015412e7330fd0501a2e54d986b0b90
Kalzangdorjelama <kalzanglama77@gmail.com>
2025-01-31 19:46:23 +0545

    add session management with NextAuth; implement user session display and sign-out functionality; wrap application in SessionProvider

--- lms/src/app/home/page.tsx
+++ lms/src/app/home/page.tsx
@@ -1,8 +1,26 @@
-"use client"
-import { signIn } from "next-auth/react";
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 function home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <Image
+          src={session.user?.image || "myphoto.png"}
+          alt="user Image"
+          width={150}
+          height={150}
+        />
+        <h1>Welcome, {session.user?.name}</h1>
+        <h3>{session.user?.email}</h3>
+        <button onClick={() => signOut}>sign Out</button>
+      </>
+    );
+  }
   return (
     <div>
+      <h1>Not Logged In</h1>
       <button onClick={() => signIn("google")}>Sign in with Google</button>
     </div>
   );
